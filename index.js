2c9535ad2b6536b3b5f9ace20d567270056b5d29
Patrick B. Buehl <pbb91@yahoo.com>
2016-07-23 20:23:48 -0500

    Add index.js with one constant variable

---
+++ index.js
@@ -0,0 +1,58 @@
+/**
+ * Created by Patrick on 7/23/2016.
+ */
+
+"use strict";
+
+/**
+ * Java Time Zone IDs
+ */
+var JAVA_TIME_ZONES =
+{
+    '-720': [],
+    '-660': [],
+    '-600': [],
+    '-540': [],
+    '-480': [],
+    '-420': [],
+    '-360': [],
+    '-300': [],
+    '-240': [],
+    '-180': [],
+    '-120': [],
+    '-60': [],
+    '0': [],
+    '60': [],
+    '120': [],
+    '180': [],
+    '240': [],
+    '300': [],
+    '360': [],
+    '420': [],
+    '480': [],
+    '540': [],
+    '600': [],
+    '660': [],
+    '720': [],
+    '800': [],
+    '860': []
+};
+
+
+
+/**
+ * Module exports.
+ * @public
+ */
+exports.getListOfTimeZones = getListOfTimeZones;
+
+/**
+ * Format object to media type.
+ *
+ * @param {offset} TimeZone offset
+ * @return {string} TimeZone ID
+ * @public
+ */
+function getListOfTimeZones(offset) {
+    return "";
+}
